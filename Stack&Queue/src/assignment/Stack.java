9dba70e3dcb992403d9db25bfc40a105ed267776
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-04-02 22:49:25 -0400

    Stack almost complete

--- Stack&Queue/src/assignment/Stack.java
+++ Stack&Queue/src/assignment/Stack.java
@@ -2,8 +2,17 @@ package assignment;
 
 public class Stack {
 
-	public static void main (String args[]){
-		System.out.println("fa");
+	public static void push(Stack st, String a){
+		Stack.push(st, new String(a));
+		System.out.println("push(" + a + ")");
+	}
+	
+	public static String pop(Stack st){
+		System.out.print("pop -> ");
+		String a = (String) Stack.pop(st);
+		System.out.println(a);
+		return a;
+		
 	}
 	
 }
